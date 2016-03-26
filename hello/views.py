43c7172ceaf334a076fde81e20dffa7716310a58
slrc1 <info@slrc1.comli.com>
2016-03-27 00:58:57 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import urllib,m3u8
 from .models import Greeting
-
+# http://morning-shelf-20604.herokuapp.com/
 # Create your views here.
 def index(request):
     vid = m3u8.load('http://dammikartmp.tulix.tv/slrc1/slrc1/playlist.m3u8')
