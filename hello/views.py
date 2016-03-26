9132498242c42f8033ae7b0e43cf1ffb8c535554
slrc1 <info@slrc1.comli.com>
2016-03-27 00:40:49 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -6,7 +6,15 @@ from .models import Greeting
 # Create your views here.
 def index(request):
     vid = m3u8.load('http://dammikartmp.tulix.tv/slrc1/slrc1/playlist.m3u8')
-    return HttpResponse(vid.playlist[0].uri)
+    fl = list()
+    for playlist in vid.playlists:
+        if playlist.uri.rstrip().endswith('.m3u8'):
+            sub = m3u8.load(playlist.uri)
+            if sub.uri.rstrip().endswith('.ts'):
+                fl.append(sub.uri)
+        elif playlist.uri.rstrip().endswith('.ts'):
+            fl.append(playlist.uri)
+    return HttpResponse(str(fl))
     return render(request, 'index.html')
 
 
