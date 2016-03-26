3e1b3bc0f975855f3fd2b7e25fb8b8354a9eb1c1
slrc1 <info@slrc1.comli.com>
2016-03-26 23:52:33 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import urllib.request
+import urllib
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    r = urllib.request.urlopen("nadeen.rf.gd/test.php")
+    r = urllib.urlopen("nadeen.rf.gd/test.php")
     r.read()
     return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
