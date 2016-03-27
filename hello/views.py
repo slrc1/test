37f349da9ebbafff40391e18c18b0955b13e0960
slrc1 <info@slrc1.comli.com>
2016-03-27 21:14:36 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -5,13 +5,7 @@ from .models import Greeting
 # http://morning-shelf-20604.herokuapp.com/
 # Create your views here.
 def index(request):
-    vid = m3u8.load('http://dammikartmp.tulix.tv/slrc1/slrc1/playlist.m3u8')
-    return HttpResponse(vid.playlists[3])
-    sub = m3u8.load('http://dammikartmp.tulix.tv/slrc1/slrc1/'+vid.playlists[0].uri)
-    fl = list()
-    for playlist in sub.playlists:
-        fl.append('http://dammikartmp.tulix.tv/slrc1/slrc1/'+playlist.uri)
-        return HttpResponse(playlist.uri)
+#   return HttpResponse(playlist.uri)
     return render(request, 'index.html')
 
 
