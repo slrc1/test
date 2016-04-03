29c1f9fc816669f2af78bbc34c58feed16136bc9
slrc1 <info@slrc1.comli.com>
2016-04-03 08:05:03 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -7,7 +7,7 @@ from .models import Greeting
 def index(request):
     tv = m3u8.tv()
     tv2 = tv.tv_init()
-    tv3 = tv.tv_load(tv2)
+#    tv3 = tv.tv_load(tv2)
     return HttpResponse(tv2)
     return render(request, 'index.html')
 
